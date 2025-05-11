ca54672451c0296483b946b80dd8cb06c585b9b4
overdrive1708 <52090022+overdrive1708@users.noreply.github.com>
2025-05-11 10:22:34 +0900

    Create project file

---
+++ src/CubicBezierViewer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CubicBezierViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ src/CubicBezierViewer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CubicBezierViewer.ViewModels
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        //--------------------------------------------------
+        // バインディングデータ
+        //--------------------------------------------------
+        /// <summary>
+        /// タイトル
+        /// </summary>
+        [ObservableProperty]
+        private string _title;
+
+        //--------------------------------------------------
+        // メソッド
+        //--------------------------------------------------
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            // タイトル表示内容の生成
+            Title = "CubicBezierViewer";
+        }
+    }
+}
